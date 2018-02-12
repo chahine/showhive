f6e286a55bd0b110d90a6455e2fd8b175ca6a5de
Chahine <chahine.m@hotmail.com>
2018-02-12 16:38:28 -0500

    Feature - Home Show Images

--- home/src/main/java/com/chahinem/showhive/home/HomeActivity.kt
+++ home/src/main/java/com/chahinem/showhive/home/HomeActivity.kt
@@ -3,15 +3,21 @@ package com.chahinem.showhive.home
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
 import com.chahinem.showhive.base.BaseActivity
+import com.chahinem.tmdb.api.TmdbApi
+import com.chahinem.tmdb.entities.Images
 import com.chahinem.trakt.api.TraktApi
 import com.chahinem.trakt.entities.Extended.NO_SEASONS
+import com.chahinem.trakt.entities.Show
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 import kotlinx.android.synthetic.main.activity_home.list
 import timber.log.Timber
 import javax.inject.Inject
 
 class HomeActivity : BaseActivity() {
 
-  @Inject lateinit var api: TraktApi
+  @Inject lateinit var traktApi: TraktApi
+  @Inject lateinit var tmdbApi: TmdbApi
   @Inject lateinit var adapter: HomeAdapter
 
   override fun getLayoutId() = R.layout.activity_home
@@ -35,10 +41,26 @@ class HomeActivity : BaseActivity() {
   override fun onResume() {
     super.onResume()
 
-    api.popular(1, 10, NO_SEASONS).subscribe({
-      val data = mutableListOf<HomeAdapter.Item>()
-      it.forEach { data.add(ShowItemView.Item(it)) }
-      adapter.swapData(data)
-    }, Timber::e)
+    // TODO: refactor into viewmodel+interactor+repo
+    traktApi.popular(1, 10, NO_SEASONS)
+        .concatMapIterable { it }
+        .filter { it.ids?.tmdb != null }
+        .concatMap {
+          val tmdbId = it.ids?.tmdb
+          when (tmdbId) {
+            null -> Observable.empty<Pair<Show, Images>>()
+            else -> tmdbApi
+                .images(tmdbId, "en")
+                .zipWith(Observable.just(it), BiFunction { a: Images, b: Show -> Pair(b, a) })
+
+          }
+        }
+        .map { (show, image) ->
+          ShowItemView.Item(
+              show.ids?.trakt,
+              image.posters?.firstOrNull()?.filePath.orEmpty())
+        }
+        .toList()
+        .subscribe(adapter::swapData, Timber::e)
   }
 }
