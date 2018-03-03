ddfaafb1888b884666a04f41b593202d7f829bab
Chahine <chahine.m@hotmail.com>
2018-03-02 23:34:24 -0500

    Chore - Dependencies Update

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,11 +1,11 @@
 object Versions {
   // Android Version
-  const val minSdkVersion = 21
+  const val minSdkVersion = 23
   const val compileSdkVersion = 27
   const val targetSdkVersion = 27
   const val buildToolsVersion = "27.0.2"
   const val gradleVersion = "3.0.1"
-  const val kotlinVersion = "1.2.21"
+  const val kotlinVersion = "1.2.30"
 
   // Libraries Version
   const val archVersion = "1.1.0"
@@ -13,11 +13,11 @@ object Versions {
   const val daggerVersion = "2.14.1"
   const val junitVersion = "4.12"
   const val leakCanaryVersion = "1.5.4"
-  const val okhttpVersion = "3.9.1"
+  const val okhttpVersion = "3.10.0"
   const val playVersion = "11.8.0"
   const val retrofitVersion = "2.3.0"
   const val rxandroidVersion = "2.0.2"
   const val rxbindingVersion = "2.1.1"
-  const val rxjavaVersion = "2.1.9"
-  const val supportLibraryVersion = "27.0.2"
+  const val rxjavaVersion = "2.1.10"
+  const val supportLibraryVersion = "27.1.0"
 }
