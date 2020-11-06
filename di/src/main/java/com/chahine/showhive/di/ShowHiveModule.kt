ae75fa9a7ab83d51fd57f9a30332fed7d5a1bdba
chahine <chahinekhan@gmail.com>
2020-11-05 23:32:48 -0600

    compiling

--- di/src/main/java/com/chahine/showhive/di/ShowHiveModule.kt
+++ di/src/main/java/com/chahine/showhive/di/ShowHiveModule.kt
@@ -13,13 +13,13 @@ import dagger.hilt.android.components.ApplicationComponent
 @InstallIn(ApplicationComponent::class)
 class ShowHiveModule {
 
-//    @Provides
-//    fun provideInputMethodService(app: Application): InputMethodManager {
-//        return app.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//    }
-//
-//    @Provides
-//    fun provideResources(app: Application): Resources {
-//        return app.resources
-//    }
+    @Provides
+    fun provideInputMethodService(app: Application): InputMethodManager {
+        return app.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
+    @Provides
+    fun provideResources(app: Application): Resources {
+        return app.resources
+    }
 }
