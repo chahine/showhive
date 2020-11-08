08a0ca1e99330753a7bd360bd466aae0ba41b6b5
chahine <chahinekhan@gmail.com>
2020-11-07 23:00:00 -0600

    done

--- home/src/main/java/com/chahine/showhive/home/calendar/CalendarInteractor.kt
+++ home/src/main/java/com/chahine/showhive/home/calendar/CalendarInteractor.kt
@@ -1,20 +1,28 @@
 package com.chahine.showhive.home.calendar
 
+import android.content.res.Resources
+import com.chahine.showhive.home.R
 import com.chahine.showhive.home.calendar.CalendarModel.CalendarCardSuccess
 import com.chahine.showhive.home.calendar.CalendarModel.CalendarFailure
 import com.chahine.showhive.home.calendar.CalendarModel.CalendarProgress
 import com.chahine.trakt.api.TraktApi
+import com.chahine.trakt.entities.CalendarShowEntry
 import io.reactivex.rxjava3.core.ObservableTransformer
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter.ofPattern
 import javax.inject.Inject
 
-class CalendarInteractor @Inject constructor(private val traktApi: TraktApi) {
+class CalendarInteractor @Inject constructor(
+    private val resources: Resources,
+    private val traktApi: TraktApi,
+) {
 
     companion object {
         // The maximum amount of days you can send is 31
         private const val DAY_WINDOW = 15
         private val DAY_FORMATTER = ofPattern("yyyy-MM-dd")
+
+        private const val SEPARATOR = " • "
     }
 
     fun calendar(): ObservableTransformer<in CalendarEvent, out CalendarModel> {
@@ -22,7 +30,7 @@ class CalendarInteractor @Inject constructor(private val traktApi: TraktApi) {
             event.switchMap {
                 val startDate = ZonedDateTime.now().minusDays(DAY_WINDOW.toLong()).format(DAY_FORMATTER)
                 traktApi
-                    .myShows(startDate, DAY_WINDOW * 2)
+                    .myShows(startDate, DAY_WINDOW * 2, "full")
                     .map { episodes ->
                         if (episodes.isEmpty()) {
                             return@map CalendarEmptyItemView.Item
@@ -32,7 +40,7 @@ class CalendarInteractor @Inject constructor(private val traktApi: TraktApi) {
                             .groupBy { it.firstAired.toLocalDate() }
                             .forEach { (date, episode) ->
                                 items += DateHeaderItemView.Item(date)
-                                items += episode.map { EpisodeItemView.Item(it) }
+                                items += episode.map { makeEpisodeItem(it) }
                             }
                         CalendarCardSuccess(items)
                     }
@@ -42,5 +50,22 @@ class CalendarInteractor @Inject constructor(private val traktApi: TraktApi) {
                     .startWithItem(CalendarProgress)
             }
         }
+    }
+
+    private fun makeEpisodeItem(item: CalendarShowEntry): EpisodeItemView.Item {
+        val show = item.show
+        val episode = item.episode
+
+        val episodeNumber = resources.getString(R.string.episode_number_format, episode.season, episode.number)
+
+        val line1 = listOfNotNull(show.title, episodeNumber).joinToString(separator = SEPARATOR)
+        val line2 = episode.title
+        val line3 = listOfNotNull(
+            show.network,
+            show.certification,
+            episode.firstAired?.format(ofPattern("HH:mm a"))
+        ).joinToString(separator = SEPARATOR)
+
+        return EpisodeItemView.Item(line1, line2, line3)
     }
 }
--- home/src/main/java/com/chahine/showhive/home/calendar/EpisodeItemView.kt
+++ home/src/main/java/com/chahine/showhive/home/calendar/EpisodeItemView.kt
@@ -5,15 +5,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.chahine.showhive.home.R
 import com.chahine.showhive.home.databinding.ItemCalendarEpisodeBinding
-import com.chahine.trakt.entities.CalendarShowEntry
-import java.time.format.DateTimeFormatter.ofPattern
 import javax.inject.Inject
 
 class EpisodeItemView {
-
-    companion object {
-        private const val SEPARATOR = " • "
-    }
 
     class Delegate @Inject constructor() : CalendarAdapter.Delegate {
 
@@ -33,19 +27,13 @@ class EpisodeItemView {
         private val binding = ItemCalendarEpisodeBinding.bind(itemView)
 
         fun bind(item: Item) = with(binding) {
-            line1.text = item.entry.show.title + SEPARATOR + itemView.resources.getString(
-                R.string.episode_number_format,
-                item.entry.episode.season,
-                item.entry.episode.number
-            )
-            line2.text = item.entry.episode.title
-            line3.text = item.entry.show.network + SEPARATOR +
-                    item.entry.show.certification + SEPARATOR +
-                    item.entry.show.firstAired!!.toLocalTime().format(ofPattern("HH:mm aa"))
+            line1.text = item.line1
+            line2.text = item.line2
+            line3.text = item.line3
         }
     }
 
-    data class Item(val entry: CalendarShowEntry) : CalendarAdapter.Item {
+    data class Item(val line1: CharSequence, val line2: CharSequence, val line3: CharSequence) : CalendarAdapter.Item {
         override fun itemViewType() = CalendarAdapter.EPISODE
     }
 }
