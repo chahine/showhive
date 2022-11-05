4ee6a830ae01628d6d4da20ba17fde3f0b9f606a
Chahine Mouhamad <chahinekhan@gmail.com>
2022-11-04 22:10:31 -0400

    Chore - Dependency Upgrades

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -7,11 +7,11 @@ object Versions {
 
     // Libraries Version
     const val chuckVersion = "1.1.0"
-    const val daggerVersion = "2.42"
+    const val daggerVersion = "2.44"
     const val glideVersion = "4.13.2"
     const val junitVersion = "4.13.2"
     const val leakCanaryVersion = "2.7"
-    const val lifecycleVersion = "2.5.0-rc01"
+    const val lifecycleVersion = "2.5.1"
     const val lottieVersion = "3.7.0"
     const val moshiVersion = "1.13.0"
     const val navigationVersion = "2.5.0-rc01"
