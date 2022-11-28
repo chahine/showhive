7096b5952870c7d0e3b1606c0272f57cce5a8a38
Chahine Mouhamad <chahinekhan@gmail.com>
2022-11-27 21:20:51 -0500

    fix lint

--- features/home/src/main/java/com/chahine/showhive/home/profile/ProfileScreen.kt
+++ features/home/src/main/java/com/chahine/showhive/home/profile/ProfileScreen.kt
@@ -3,12 +3,15 @@ package com.chahine.showhive.home.profile
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -88,10 +92,17 @@ fun ProfileUserCard(userCard: ProfileItem.UserCard, signOutListener: () -> Unit)
             )
         }
         userCard.user.location?.let {
-            Text(
-                text = it,
-                style = MaterialTheme.typography.bodyMedium,
-            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    modifier = Modifier.padding(2.dp),
+                    painter = painterResource(R.drawable.ic_baseline_location_on),
+                    contentDescription = null,
+                )
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
         }
         userCard.user.about?.let {
             Text(
